39a3ced93e5ef0f19aa742d2222940df351eae7b
poopoofly <b05303008@ntu.edu.tw>
2019-06-04 15:43:56 +0800

    規則+視窗+第二關的箭頭們

--- 04第二關遊戲視窗.py
+++ 04第二關遊戲視窗.py
@@ -34,6 +34,23 @@ score2_rect = score2_surface.get_rect()
 score2_rect.center = (width-20-size-50, 50)
 
 # 設定題目
+image_up_before = pygame.image.load('上前.png')
+image_up_after = pygame.image.load('上後.png')
+image_down_before = pygame.image.load('下前.png')
+image_down_after = pygame.image.load('下後.png')
+image_left_before = pygame.image.load('左前.png')
+image_left_after = pygame.image.load('左後.png')
+image_right_before = pygame.image.load('右前.png')
+image_right_after = pygame.image.load('右後.png')
+image_up_before_reverse = pygame.image.load('上前反.png')
+image_up_after_reverse = pygame.image.load('上後反.png')
+image_down_before_reverse = pygame.image.load('下前反.png')
+image_down_after_reverse = pygame.image.load('下後反.png')
+image_left_before_reverse = pygame.image.load('左前反.png')
+image_left_after_reverse = pygame.image.load('左後反.png')
+image_right_before_reverse = pygame.image.load('右前反.png')
+image_right_after_reverse = pygame.image.load('右後反.png')
+image_cover = pygame.image.load('遮罩.png')
 ### 隨機出現上下左右的圖
 
 while True: # 遊戲迴圈
@@ -50,8 +67,61 @@ while True: # 遊戲迴圈
     screen.blit(score2_surface, score2_rect) # 玩家2分數
 
     # 顯示題目
-    pygame.draw.rect(screen, orange, (20, 220, width/2-40, 350), 2) # 玩家1題目框框
-    pygame.draw.rect(screen, orange, (width/2+20, 220, width/2-40, 350), 2) # 玩家2題目框框
+    pygame.draw.rect(screen, orange, (20, 220, width/2-40, 350), 2) # 玩家1題目框框 310*350
+    pygame.draw.rect(screen, orange, (width/2+20, 220, width/2-40, 350), 2) # 玩家2題目框框 310*350
+    # 玩家1的圖示
+    screen.blit(image_up_before, (30, 223)) # 1-1
+    screen.blit(image_up_before, (30 + 64 + 11.3, 223)) # 1-2
+    screen.blit(image_up_before, (30 + 64 + 64 + 11.3 + 11.3, 223)) # 1-3
+    screen.blit(image_up_before, (30 + 64 + 64 + 64 + 11.3 + 11.3 + 11.3, 223)) # 1-4
+    screen.blit(image_cover, (20, 220)) # 第一行遮罩 #好像用不到哈哈哈
+    screen.blit(image_up_before, (30, 220 + 70 + 3)) # 2-1
+    screen.blit(image_up_before, (30 + 64 + 11.3, 220 + 70 + 3)) # 2-2
+    screen.blit(image_up_before, (30 + 64 + 64 + 11.3 + 11.3, 220 + 70 + 3)) # 2-3
+    screen.blit(image_up_before, (30 + 64 + 64 + 64 + 11.3 + 11.3 + 11.3, 220 + 70 + 3)) # 2-4
+    screen.blit(image_cover, (20, 220+70)) # 第二行遮罩
+    screen.blit(image_up_before, (30, 220 + 70 + 70 + 3)) # 3-1
+    screen.blit(image_up_before, (30 + 64 + 11.3, 220 + 70 + 70 + 3)) # 3-2
+    screen.blit(image_up_before, (30 + 64 + 64 + 11.3 + 11.3, 220 + 70 + 70 + 3)) # 3-3
+    screen.blit(image_up_before, (30 + 64 + 64 + 64 + 11.3 + 11.3 + 11.3, 220 + 70 + 70 + 3)) # 3-4
+    screen.blit(image_cover, (20, 220+70+70)) # 第三行遮罩
+    screen.blit(image_up_before, (30, 220 + 70 + 70 + 70 + 3)) # 4-1
+    screen.blit(image_up_before, (30 + 64 + 11.3, 220 + 70 + 70 + 70 + 3)) # 4-2
+    screen.blit(image_up_before, (30 + 64 + 64 + 11.3 + 11.3, 220 + 70 + 70 + 70 + 3)) # 4-3
+    screen.blit(image_up_before, (30 + 64 + 64 + 64 + 11.3 + 11.3 + 11.3, 220 + 70 + 70 + 70 + 3)) # 4-4
+    screen.blit(image_cover, (20, 220+70+70+70)) # 第四行遮罩
+    screen.blit(image_up_before, (30, 220 + 70 + 70 + 70 + 70 + 3)) # 5-1
+    screen.blit(image_up_before, (30 + 64 + 11.3, 220 + 70 + 70 + 70 + 70 + 3)) # 5-2
+    screen.blit(image_up_before, (30 + 64 + 64 + 11.3 + 11.3, 220 + 70 + 70 + 70 + 70 + 3)) # 5-3
+    screen.blit(image_up_before, (30 + 64 + 64 + 64 + 11.3 + 11.3 + 11.3, 220 + 70 + 70 + 70 + 70 + 3)) # 5-4
+    screen.blit(image_cover, (20, 220+70+70+70+70)) # 第五行遮罩
+    # 玩家2的圖示
+    screen.blit(image_up_before, (30 + 350, 223)) # 1-1
+    screen.blit(image_up_before, (30 + 64 + 11.3 + 350, 223)) # 1-2
+    screen.blit(image_up_before, (30 + 64 + 64 + 11.3 + 11.3 + 350, 223)) # 1-3
+    screen.blit(image_up_before, (30 + 64 + 64 + 64 + 11.3 + 11.3 + 11.3 + 350, 223)) # 1-4
+    screen.blit(image_cover, (20+350, 220)) # 第一行遮罩 #好像用不到哈哈哈
+    screen.blit(image_up_before, (30 + 350, 220 + 70 + 3)) # 2-1
+    screen.blit(image_up_before, (30 + 64 + 11.3 + 350, 220 + 70 + 3)) # 2-2
+    screen.blit(image_up_before, (30 + 64 + 64 + 11.3 + 11.3 + 350, 220 + 70 + 3)) # 2-3
+    screen.blit(image_up_before, (30 + 64 + 64 + 64 + 11.3 + 11.3 + 11.3 + 350, 220 + 70 + 3)) # 2-4
+    screen.blit(image_cover, (20+350, 220+70)) # 第二行遮罩
+    screen.blit(image_up_before, (30 + 350, 220 + 70 + 70 + 3)) # 3-1
+    screen.blit(image_up_before, (30 + 64 + 11.3 + 350, 220 + 70 + 70 + 3)) # 3-2
+    screen.blit(image_up_before, (30 + 64 + 64 + 11.3 + 11.3 + 350, 220 + 70 + 70 + 3)) # 3-3
+    screen.blit(image_up_before, (30 + 64 + 64 + 64 + 11.3 + 11.3 + 11.3 + 350, 220 + 70 + 70 + 3)) # 3-4
+    screen.blit(image_cover, (20+350, 220+70+70)) # 第三行遮罩
+    screen.blit(image_up_before, (30 + 350, 220 + 70 + 70 + 70 + 3)) # 4-1
+    screen.blit(image_up_before, (30 + 64 + 11.3 + 350, 220 + 70 + 70 + 70 + 3)) # 4-2
+    screen.blit(image_up_before, (30 + 64 + 64 + 11.3 + 11.3 + 350, 220 + 70 + 70 + 70 + 3)) # 4-3
+    screen.blit(image_up_before, (30 + 64 + 64 + 64 + 11.3 + 11.3 + 11.3 + 350, 220 + 70 + 70 + 70 + 3)) # 4-4
+    screen.blit(image_cover, (20+350, 220+70+70+70)) # 第四行遮罩
+    screen.blit(image_up_before, (30 + 350, 220 + 70 + 70 + 70 + 70 + 3)) # 5-1
+    screen.blit(image_up_before, (30 + 64 + 11.3 + 350, 220 + 70 + 70 + 70 + 70 + 3)) # 5-2
+    screen.blit(image_up_before, (30 + 64 + 64 + 11.3 + 11.3 + 350, 220 + 70 + 70 + 70 + 70 + 3)) # 5-3
+    screen.blit(image_up_before, (30 + 64 + 64 + 64 + 11.3 + 11.3 + 11.3 + 350, 220 + 70 + 70 + 70 + 70 + 3)) # 5-4
+    screen.blit(image_cover, (20+350, 220+70+70+70+70)) # 第五行遮罩
+
 
     for event in pygame.event.get():
         # 玩家按下相對按鍵，題目變色
